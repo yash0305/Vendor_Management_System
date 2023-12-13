607ce3b903aa2e4fd537de0375f5b3180d4856e1
yash0305 <kesharwaniyash20@gmail.com>
2023-12-13 21:02:17 +0530

    added model of Vendor, PurchaseOrder, HistoricalPerformance and thier admin site code

---
+++ VendorManagementSystem/api/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Vendor, PurchaseOrder, HistoricalPerformance
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'vendor_code', 'on_time_delivery_rate', 'quality_rating_avg', 'fulfillment_rate')
+    search_fields = ('name', 'vendor_code')
+
+@admin.register(PurchaseOrder)
+class PurchaseOrderAdmin(admin.ModelAdmin):
+    list_display = ('po_number', 'vendor', 'order_date', 'status', 'delivery_date')
+    list_filter = ('vendor', 'status')
+    search_fields = ('po_number', 'vendor__name')
+
+
+@admin.register(HistoricalPerformance)
+class HistoricalPerformanceAdmin(admin.ModelAdmin):
+    list_display = ('vendor_name', 'date', 'on_time_delivery_rate', 'quality_rating_avg', 'fulfillment_rate')
+    list_filter = ('vendor__name', 'date')
+
+    def vendor_name(self, obj):
+        return obj.vendor.name if obj.vendor else ''  # Return the vendor's name or empty string if vendor is None
+
+    vendor_name.admin_order_field = 'vendor__name'  # Enable ordering by vendor name
---
+++ VendorManagementSystem/api/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+import uuid
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+
+    class Meta:
+        abstract = True
+
+
+class Vendor(BaseModel):
+    name = models.CharField(max_length=255)
+    contact_details = models.TextField()
+    address = models.TextField()
+    vendor_code = models.CharField(unique=True, max_length=50)
+    on_time_delivery_rate = models.FloatField(default=0)
+    quality_rating_avg = models.FloatField(default=0)
+    average_response_time = models.FloatField(default=0)
+    fulfillment_rate = models.FloatField(default=0)
+
+class PurchaseOrder(models.Model):
+    po_number = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='purchase_order')
+    po_number = models.CharField(max_length=100, unique=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=100)
+    quality_rating = models.FloatField(null=True, blank=True)
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField(null=True, blank=True)
+
+class HistoricalPerformance(BaseModel):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='historical_performance')
+    date = models.DateTimeField()
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()
+
+    # def __str__(self):
+    #     return f"{self.vendor.name}"
